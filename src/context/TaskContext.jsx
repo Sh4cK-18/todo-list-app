48037a9579ce25b39ea5ba9cf77a7e6e6e2fe680
Sh4cK-Dev <102748155+Sh4cK-Dev@users.noreply.github.com>
2023-10-09 22:27:49 -0500

    stable version

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -16,7 +16,7 @@ export const TaskProvider = ({ children }) => {
   const [selectedTask, setSelectedTask] = useState(null);
 
   async function loadTasks() {
-    const res = await fetch("http://localhost:3000/api/tasks");
+    const res = await fetch("/api/tasks");
     const data = await res.json();
     setTasks(data);
   }
