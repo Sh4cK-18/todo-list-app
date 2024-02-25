90d99a42300b2d645d6e8c1edee7d674e82c539e
Sh4cK-Dev <102748155+Sh4cK-Dev@users.noreply.github.com>
2024-02-24 22:56:27 -0500

    Agrega variables de entorno y actualiza dependencias

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,4 +1,6 @@
 import { useTasks } from "@/context/TaskContext";
+import { IconEdit } from '@tabler/icons-react';
+import { IconTrashX } from '@tabler/icons-react';
 
 function TaskCard({ task }) {
 
@@ -9,14 +11,16 @@ function TaskCard({ task }) {
       <div>
         <h1 className="text-2xl font-bold">{task.title}</h1>
         <p>{task.content}</p>
-        <div className="flex gap-x-2">
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
+        <div className="flex gap-x-2 mt-2">
+          <button onClick={() => deleteTask(task.id)}>
+            <IconTrashX size={35} />
+          </button>
           <button
             onClick={() => {
               setSelectedTask(task);
             }}
           >
-            Edit
+            <IconEdit size={35} />
           </button>
         </div>
       </div>
